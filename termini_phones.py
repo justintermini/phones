03df0b774abfdae72168bff27973277c5591aa4b
justintermini <justintermini@gmail.com>
2020-09-23 12:49:50 -0400

    add comments

--- termini_phones.py
+++ termini_phones.py
@@ -10,10 +10,14 @@ def last_only():
         if last_name in line:
             print(line.title())
 
+# Open phone file in 'read' format
 f = open("phones.txt", 'r')
+# Prompt user for name
 name = input("Enter a last name, or first and last name: ")
+# Convert name to lowercase format
 name = name.lower()
 
+# Split name into one or two parts
 name_list = name.split()
 
 if len(name_list) == 2:
@@ -29,11 +33,11 @@ elif len(name_list) > 2:
     print("Error: Please list just one name.")
 
 
-# Output for testing
-#print(name_list)
-#print(name_list[0])
-#print(len(name_list))
-#print(last_name.title())
+Output for testing
+print(name_list)
+print(name_list[0])
+print(len(name_list))
+print(last_name.title())
 
 
 
